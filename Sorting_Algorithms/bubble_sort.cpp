d81d32ae722766dad87c1c7236170f527c97d995
Sabareeshwaran V <sabareeshwaran1302@gmail.com>
2026-09-23 23:26:19 +0530

    Add files via upload

---
+++ Sorting_Algorithms/bubble_sort.cpp
@@ -0,0 +1,49 @@
+/*
+Bubble Sort :
+
+Bubble Sort is a sorting algorithm that repeatedly compares adjacent elements and 
+swaps them if they are in the wrong order.
+
+In simple words:
+Compare neighbors → swap if wrong → repeat.
+
+The largest element gradually moves to the end, like a bubble rising to the top.
+
+*/
+
+// Bubble Sort for Ascending Order  : 
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Bubble Sort for Descending Order  :
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] < arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
